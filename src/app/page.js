b8af5ff71476cee17837959052f56ed3790321fd
Thorbaris <67578936+Thorbaris@users.noreply.github.com>
2024-06-25 00:21:51 -0400

    init

--- src/app/page.js
+++ src/app/page.js
@@ -1,112 +1,151 @@
-import Image from "next/image";
+"use client"; 
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import { useState } from "react";
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 export default function Home() {
+  const [gr, setGrMl] = useState(0);
+  const [precio, setPrecio] = useState(0);
+  const [precioKiloLitro, setPrecioKilo] = useState(0);
+
+  const [cantidad_rollos, setCantidadRollos] = useState(0);
+  const [metros_rollo, setMetrosRollo] = useState(0);
+  const [precioConfort, setPrecioConfort] = useState(0);
+  const [precioPorMetroConfort, setPrecioMetroConfort] = useState(0);
+
+  const handleChange = (event) => {
+    setGrMl(Number(event.target.value));
+  };
+  const handlePrecio = (event) => {
+    setPrecio(Number(event.target.value));
+  }
+  const handlePrecioKilo = (event) => {
+    setPrecioKilo(Number(event.target.value));
+  }
+  const calcularTotalKiloLitro = () => {
+    setPrecioKilo(1000/gr * precio);
+  }
+
+  const handleChangeCantidadRollos = (event) => {
+    setCantidadRollos(Number(event.target.value));
+  }
+  const handleChangeMetrosRollo = (event) => {
+    setMetrosRollo(Number(event.target.value));
+  }
+  const handlePrecioConfort = (event) => {
+    setPrecioConfort(Number(event.target.value));
+  }
+  const handlePrecioMetroConfort = (event) => {
+    setPrecioMetroConfort(Number(event.target.value));
+  }
+
+  const calcularTotalMetroConfort = () => {
+    console.log(cantidad_rollos, metros_rollo, precioConfort);
+    setPrecioMetroConfort( (precioConfort / (cantidad_rollos * metros_rollo)).toFixed(2) );
+  }
+
+  const limpiarKg = () => {
+    setGrMl(0);
+    setPrecio(0);
+    setPrecioKilo(0);
+  }
+
+  const limpiarConfort = () => {
+    setCantidadRollos(0);
+    setMetrosRollo(0);
+    setPrecioConfort(0);
+    setPrecioMetroConfort(0);
+  }
+  
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <main className="px-4">
+      <h1 className="my-4 mb-7 text-center text-slate-400">Asistente de compras</h1>
+      <div className="flex flex-col gap-4">
+        <section className="flex flex-col gap-2 my-3">
+          <div className="flex gap-2 mb-2">
+            <h4>Calcular precio por Kg/Lt</h4>
+              <DeleteIcon className="text-red-600"  onClick={limpiarKg}/>
+          </div>
+          <div className="flex gap-2">
+            <TextField
+              value={gr}
+              onChange={handleChange}
+              id="outlined-basic"
+              label="gr / ml"
+              variant="outlined"
+              type="number" // Asegúrese de que el campo solo acepte números
+              style={{ backgroundColor: "white" }}
             />
-          </a>
-        </div>
-      </div>
+            <TextField
+              id="outlined-basic"
+              label="Precio"
+              variant="outlined"
+              value={precio}
+              style={{ backgroundColor: "white" }}
+              onChange={handlePrecio}
+            />
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+            <Button onClick={ calcularTotalKiloLitro } variant="contained" color="primary">
+              Calcular
+            </Button>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+            <TextField
+              id="outlined-basic"
+              label="Precio por kilo / litro"
+              variant="outlined"
+              value={precioKiloLitro}
+              style={{ backgroundColor: "white" }}
+              disabled
+            />
+          </div>
+        </section>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <section className="flex flex-col gap-2 my-3">
+          <div className="flex gap-2 mb-2">
+            <h4>Calcular precio por metro de confort</h4>
+            <DeleteIcon className="text-red-600"  onClick={limpiarConfort}/>
+          </div>
+          <div className="flex gap-2">
+            <TextField
+              id="outlined-basic"
+              label="Cantida de rollos"
+              variant="outlined"
+              type="number" // Asegúrese de que el campo solo acepte números
+              value={cantidad_rollos}
+              onChange={handleChangeCantidadRollos}
+              style={{ backgroundColor: "white" }}
+            />
+            <TextField
+              id="outlined-basic"
+              label="Metros por rollo"
+              variant="outlined"
+              value={metros_rollo}
+              onChange={handleChangeMetrosRollo}
+              style={{ backgroundColor: "white" }}
+            />
+            <TextField
+              id="outlined-basic"
+              label="Precio"
+              variant="outlined"
+              value={precioConfort}            
+              onChange={handlePrecioConfort}
+              style={{ backgroundColor: "white" }}
+            />
+            <Button onClick={ calcularTotalMetroConfort } variant="contained" color="primary">
+              Calcular
+            </Button>
+            <TextField
+              id="outlined-basic"
+              label="Precio por metro de confort"
+              variant="outlined"
+              value={precioPorMetroConfort}
+              style={{ backgroundColor: "white" }}
+              disabled
+            />
+          </div>
+        </section>
       </div>
     </main>
   );
